f57931f11dab1b324df4b0a255c157277803a117
David Saeva <drsaeva@gmail.com>
2019-05-21 12:45:04 -0400

    fix dice bug to properly sample 1:10

--- src/roller/dice.ts
+++ src/roller/dice.ts
@@ -1,11 +1,11 @@
-import { isNumber, range, sample, times } from 'lodash';
+import { range, sample, times } from 'lodash';
 
 /**
  * Roll a d10
  * @returns {number} returns a number between 1 and 10
  */
 export const d10 = (): number => {
-  const roll: number | undefined = sample(range(1, 10));
+  const roll: number | undefined = sample(range(1,11));
   const result: number = (roll) ? roll : -1 ;
   if (result === -1) throw new Error('dice roll cannot return nil values!');
   else return result;
@@ -17,8 +17,8 @@ export const d10 = (): number => {
  * @returns {Array<number>} the result of the dice roll
  */
 export const rollDice = (numDice: number): Array<number> => {
-  if (!isNumber(numDice) || numDice <= 0)
-    throw new Error("numDice must be a positive number");
+  if ( Number.isNaN(numDice) || ! Number.isSafeInteger(numDice) || numDice <= 0)
+    throw new Error("numDice must be a positive integer representing an amount of dice");
   const result: Array<number> =  times(numDice, d10);
 
   return result;
