0c143fa8dba2588b307c80f7977cb9ffcf6a98ef
David Saeva <drsaeva@gmail.com>
2019-05-21 12:44:12 -0400

    fix sort bug by changing order of eval

--- src/roller/sort.ts
+++ src/roller/sort.ts
@@ -1,10 +1,10 @@
-import { includes } from 'lodash';
+import { isUndefined } from 'lodash';
 
 export const sort = (rolls: Array<number>): Array<number> => {
   const sortedRollSet: Array<number> = [];
   for (let roll of rolls) {
-    if (includes(sortedRollSet, roll)) sortedRollSet[roll] = sortedRollSet[roll] + 1;
-    else sortedRollSet[roll] = 1;
+    if (isUndefined(sortedRollSet[roll])) sortedRollSet[roll] = 1;
+    else sortedRollSet[roll] = sortedRollSet[roll] + 1;
   }
   return sortedRollSet;
 }
